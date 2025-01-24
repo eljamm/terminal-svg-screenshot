b184e0e45538eee08a19d70d6049d1d58944899e
suin <suinyeze@gmail.com>
2025-01-24 22:19:28 +0900

    chore: add nix flake for dev environment
    Signed-off-by: suin <suinyeze@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        tools = with pkgs; [
+          tmux
+          charm-freeze
+          fish
+          inkscape
+          fontconfig
+          nerd-fonts.jetbrains-mono
+        ];
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = tools;
+            FONTCONFIG_PATH = "${pkgs.fontconfig.out}/etc/fonts";
+          };
+          start = pkgs.mkShell {
+            packages = tools;
+            shellHook = ''
+              ./start.sh
+            '';
+          };
+        };
+      }
+    );
+}
